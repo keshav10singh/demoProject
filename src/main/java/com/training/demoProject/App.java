c770e2f1b744bbc8a85e60edcec93ea20b56e509
Keshav Singh <keenu@Keshavs-MacBook-Pro.local>
2022-09-22 22:21:04 +0530

    First Script

---
+++ src/main/java/com/training/demoProject/App.java
@@ -0,0 +1,13 @@
+package com.training.demoProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
